c30bb46f16eb23c0dabd3fe127083f250e2dc814
roalchaq <roalchaq@gmail.com>
2018-07-20 18:01:05 +0100

    Fixed bug for Parsing Messages in Play mode

--- Scripts/RosMessenger.cs
+++ Scripts/RosMessenger.cs
@@ -211,7 +211,7 @@ public class RosMessenger : HoloToolkit.Unity.Singleton<RosMessenger>
 
         //con = true;
 
-        //Socket.OnMessage +=Editor_MessageRecieved;
+        Socket.OnMessage +=Editor_MessageRecieved;
         while (true)
         {
             Thread.Sleep(10000);
